c81bc68e60ee239a81ceaaa50a0ed8ad8cdd4fa9
Cascallares Gabriel <git@github.com:gcascallares/tpDiscoProgBas2.git>
2022-10-23 15:51:39 -0300

    Add GenreService

---
+++ BlockBuster/BlockBuster/Repositories/GenreService.cs
@@ -0,0 +1,42 @@
+﻿using BlockBuster.Repositories.Interfaces;
+using BlockBuster.Services.Interfaces.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlockBuster.Repositories
+{
+    public class GenreService : IGenericServices<Genre>
+    {
+        private GenericRepository<Genre> _genreRepository;
+        public GenreService(IGenericRepository<Movie> movieRepository)
+        {
+            this._genreRepository = new GenericRepository<Genre>();
+        }
+        public void Add(Genre entity)
+        {
+            _genreRepository.Add(entity);
+        }
+
+        public void Delete(int entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Genre> GetAll()
+        {
+            return _genreRepository.GetAll();
+        }
+
+        public Genre GetById(int entiyId)
+        {
+            return _genreRepository.GetById(entiyId);
+        }
+
+        public void Update(Genre entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
