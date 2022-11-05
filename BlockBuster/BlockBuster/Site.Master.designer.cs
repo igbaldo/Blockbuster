763be01c6812cde0d4e7deb5b1383dfe9514b9fc
Tomas Nuñez <tomass.nunez@gmail.com>
2022-11-05 02:57:52 -0300

    cambios front home y películas

--- BlockBuster/BlockBuster/Site.Master.designer.cs
+++ BlockBuster/BlockBuster/Site.Master.designer.cs
@@ -15,13 +15,13 @@ namespace BlockBuster
     {
 
         /// <summary>
-        /// Control Image1.
+        /// Control Image2.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Image1;
+        protected global::System.Web.UI.WebControls.Image Image2;
 
         /// <summary>
         /// Control MainContent.
--- BlockBuster/BlockBuster/Views/Default.aspx.designer.cs
+++ BlockBuster/BlockBuster/Views/Default.aspx.designer.cs
@@ -22,14 +22,5 @@ namespace BlockBuster
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ListView ListView1;
-
-        /// <summary>
-        /// Control movieList.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ListView movieList;
     }
 }
